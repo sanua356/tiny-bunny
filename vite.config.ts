034dfb3bebbbabbffdb9f9e6702436f72dc893fc
Alexander <sanek.pankratov356@gmail.com>
2024-08-09 12:45:12 +0300

    fix: change config for fix assets paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-deploy-demo/',
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
