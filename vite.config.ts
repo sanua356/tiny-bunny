329ab36e67eaf768cda7108931276a3a19c0a2d3
Alexander <sanek.pankratov356@gmail.com>
2024-08-09 12:49:08 +0300

    fix: assets paths fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from "url";
 import react from '@vitejs/plugin-react-swc';
+import { fileURLToPath, URL } from "url";
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-deploy-demo/',
+  base: '/tiny-bunny/',
   resolve: {
     alias: [
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
